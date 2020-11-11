17003679f9189e31d7b879afbef7b4ce7c1d7389
hauptspieler <baisch474@gmail.com>
2020-11-11 19:07:55 -0300

    18

--- app/database/db.php
+++ app/database/db.php
@@ -10,6 +10,20 @@ function dd($value)
 
 }
 
+
+function executeQuery($sql, $data)
+{
+    global $conn;
+    $conn-> prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s', count($values));             // faz a injection de values de uma forma mais segura 
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    return $stmt;
+}
+
+
+
 function selectAll($table, $conditions = [])
 {
         global $conn;
@@ -32,11 +46,7 @@ function selectAll($table, $conditions = [])
             }
            
            
-            $stmt = $conn-> prepare($sql);
-            $values = array_values($conditions);
-            $types = str_repeat('s', count($values));             // faz a injection de values de uma forma mais segura 
-            $stmt->bind_param($types, ...$values);
-            $stmt->execute();
+            $stmt = executeQuery($sql, $conditions)
             $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
             return $records;
        
@@ -45,3 +55,95 @@ function selectAll($table, $conditions = [])
 }
 
 
+function selectOne($table, $conditions = [])
+  {
+        global $conn;
+        $sql = "SELECT * FROM $table";
+        
+            // retornar apenas as condicoes dadas
+             $i = 0;
+            foreach ($conditions as $key => $value) {
+               if ($i === 0){
+                $sql = $sql . "WHERE $key=?";    // faz a "injeção" de values de uma forma mais segura 
+               } else {
+                    $sql = $sql . " AND $key=?";    // faz a "injeção" de values de uma forma mais segura 
+                }
+                $i++;
+            }
+
+            $sql - $sql . " LIMIT 1 ";
+           
+           
+            $stmt = executeQuery($sql, $conditions);
+            $records = $stmt->get_result()->fetch_assoc();
+            return $records;
+       
+    
+
+}
+ 
+
+function create($table, $data){
+    
+    global $conn;
+
+    $sql= "INSERT INTO $table SET";
+
+
+    $i = 0;
+    foreach ($conditions as $key => $value) {
+       if ($i === 0){
+        $sql = $sql . "WHERE $key=?";    // faz a "injeção" de values de uma forma mais segura 
+       } else {
+            $sql = $sql . " AND $key=?";    // faz a "injeção" de values de uma forma mais segura 
+        }
+        $i++;
+    }
+
+    $stmt = executeQuery($sql, $data);
+    $id = $stmt->insert_id;
+    return $id;
+
+}
+
+
+function update($table, $id, $data){
+    
+    global $conn;
+
+    $sql= "UPDATE INTO $table SET";
+
+
+    $i = 0;
+    foreach ($conditions as $key => $value) {
+       if ($i === 0){
+        $sql = $sql . "WHERE $key=?";    // faz a "injeção" de values de uma forma mais segura 
+       } else {
+            $sql = $sql . " AND $key=?";    // faz a "injeção" de values de uma forma mais segura 
+        }
+        $i++;
+    }
+
+    $sql = $sql . "WHERE id=?";
+    $data['id'] = $id;
+    $stmt = executeQuery($sql, $data);
+    return $stmt->affected_rows;
+
+}
+
+function delete($table, $id){
+    
+    global $conn;
+
+    $sql= "DELETE INTO $table SET";
+
+    $
+    $stmt = executeQuery($sql, ['id' => $id]);
+    return $stmt->affected_rows;
+
+}
+
+
+
+// https://www.youtube.com/watch?v=-yV1DCLcqyM&list=PL3pyLl-dgiqD0eKYJ-XSxrHaRh-zsA2tP&index=18&ab_channel=AwaMelvine
+// video 18
